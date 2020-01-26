4cd4d28710c1703e867734c2192addd5e289871e
Timo Teräs <timo.teras@iki.fi>
2020-01-26 11:55:51 +0200

    remove tests connected to help output validation
    The help has been moved to man pages now.

--- test/command-parsing.sh
+++ test/command-parsing.sh
@@ -9,16 +9,6 @@ if [ "$help_output" != "$invalid_option_output" ]; then
 	fail=$((fail+1))
 fi
 
-if ! ../src/apk --help 2>/dev/null | grep -q "^Use apk --help --verbose for a full command listing."; then
-	echo "FAIL: brief help gives long help"
-	fail=$((fail+1))
-fi
-
-if ../src/apk --help --verbose 2>/dev/null | grep -q "^Use apk --help --verbose for a full command listing."; then
-	echo "FAIL: long help does not work"
-	fail=$((fail+1))
-fi
-
 if [ $fail -eq 0 ]; then
 	echo "OK: command parsing works"
 fi
