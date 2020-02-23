63dceba116049b82f356a6cf9534ab218750f339
Jakub Jirutka <jakub@jirutka.cz>
2020-02-23 18:01:43 +0100

    fix test6.sh - linux-vanilla was renamed to linux-lts in v3.11

--- test/test6.sh
+++ test/test6.sh
@@ -3,9 +3,9 @@
 # desc: test triggers in kernel package
 
 $APK add --root $ROOT --initdb -U --repository $PWD/repo1 \
-	--repository $SYSREPO alpine-keys linux-vanilla
+	--repository $SYSREPO alpine-keys linux-lts
 
-test -e "$ROOT"/boot/vmlinuz-vanilla
+test -e "$ROOT"/boot/vmlinuz-lts
 
-test -e "$ROOT"/boot/initramfs-vanilla
+test -e "$ROOT"/boot/initramfs-lts
 
