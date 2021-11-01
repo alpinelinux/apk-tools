054962bf00a78a0827da7f004cca78b54290a73d
Timo Teräs <timo.teras@iki.fi>
2021-11-01 14:28:46 +0200

    db: handle waitpid returning EINTR
    (cherry picked from commit 1bd6ea206e7bed88be21db7a99521df7bf798bb9)

--- src/database.c
+++ src/database.c
@@ -1967,7 +1967,7 @@ int apk_db_run_script(struct apk_database *db, char *fn, char **argv)
 		execve(fn, argv, environment);
 		exit(127); /* should not get here */
 	}
-	waitpid(pid, &status, 0);
+	while (waitpid(pid, &status, 0) < 0 && errno == EINTR);
 	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
 		apk_error("%s: script exited with error %d", basename(fn), WEXITSTATUS(status));
 		return -1;
