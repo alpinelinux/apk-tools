af6e2c757e193006e303c9377b37d817364eda7a
Timo Teräs <timo.teras@iki.fi>
2022-12-20 15:32:49 +0200

    apk: improve interactive mode handling
    - implement and document --no-interactive
    - improve --interactive documentation
    - treat EOF as Y
    
    fixes #10860

--- src/apk.c
+++ src/apk.c
@@ -71,6 +71,7 @@ static void version(struct apk_out *out, const char *prefix)
 	OPT(OPT_GLOBAL_interactive,		APK_OPT_SH("i") "interactive") \
 	OPT(OPT_GLOBAL_keys_dir,		APK_OPT_ARG "keys-dir") \
 	OPT(OPT_GLOBAL_no_cache,		"no-cache") \
+	OPT(OPT_GLOBAL_no_interactive,		"no-interactive") \
 	OPT(OPT_GLOBAL_no_logfile,		"no-logfile") \
 	OPT(OPT_GLOBAL_no_network,		"no-network") \
 	OPT(OPT_GLOBAL_no_progress,		"no-progress") \
@@ -155,6 +156,9 @@ static int option_parse_global(void *ctx, struct apk_ctx *ac, int opt, const cha
 		break;
 	case OPT_GLOBAL_interactive:
 		ac->flags |= APK_INTERACTIVE;
+		break;
+	case OPT_GLOBAL_no_interactive:
+		ac->flags &= ~APK_INTERACTIVE;
 		break;
 	case OPT_GLOBAL_preserve_env:
 		ac->flags |= APK_PRESERVE_ENV;
