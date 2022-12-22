b75ffff1551cf100e879e5aca3404b947a540581
Timo Teräs <timo.teras@iki.fi>
2022-12-22 15:12:01 +0200

    apk: ignore SIGINT after applet is executed
    apk_db_close is not re-entrant, and would be executed by the signal
    handler. It makes sense to just ignore the signal after applet
    is complete as the clean up work has been started already.
    
    fixes #10840

--- src/apk.c
+++ src/apk.c
@@ -557,6 +557,7 @@ int main(int argc, char **argv)
 	memcpy(args->item, argv, argc * sizeof(*argv));
 
 	r = applet->main(ctx, &db, args);
+	signal(SIGINT, SIG_IGN);
 	apk_db_close(&db);
 
 #ifdef TEST_MODE
