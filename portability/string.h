4c7aa85da11b467c1dcc043b6bd033d26ec38230
Timo Teräs <timo.teras@iki.fi>
2024-03-21 12:45:57 +0200

    portability: add strchrnul

--- portability/string.h
+++ portability/string.h
@@ -4,6 +4,12 @@
 extern void *memrchr(const void *m, int c, size_t n);
 #endif
 
+#ifdef NEED_STRCHRNUL
+static inline char *strchrnul(const char *s, int c) {
+	return strchr(s, c) ?: s + strlen(s);
+}
+#endif
+
 #ifdef NEED_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t size);
 #endif
