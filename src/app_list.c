3053763e1d0f6a6ffcfbb8edb38ea04caa9c1bed
Timo Teräs <timo.teras@iki.fi>
2024-03-25 18:21:54 +0200

    list: consider only tag appropriate versions in upgradable
    consider package as upgradable only if it is from the same pinned
    repository. clean up is_upgradable() while at it.
    
    fixes #10899
    
    (cherry picked from commit 2ebc88147fe7f6b8a7cb58e201f6470960fc0240)
    (cherry picked from commit c7af1799dcbbaaaa9056e6ca1a8547256646dc92)

--- src/app_list.c
+++ src/app_list.c
@@ -62,34 +62,23 @@ static int is_orphaned(const struct apk_name *name)
 	return (repos & ~BIT(APK_REPOSITORY_CACHED)) == 0;
 }
 
-/* returns the currently installed package if there is a newer package that satisfies `name` */
-static const struct apk_package *is_upgradable(struct apk_name *name, const struct apk_package *pkg0)
+/* returns the currently installed package if 'pkg' is a newer and installable version */
+static const struct apk_package *is_upgradable(const struct apk_database *db, const struct apk_package *pkg)
 {
-	struct apk_provider *p;
+	struct apk_name *name = pkg->name;
 	struct apk_package *ipkg;
-	apk_blob_t no_version = APK_BLOB_STR("");
-	apk_blob_t *latest = &no_version;
-	int r;
-
-	if (!name) return NULL;
+	unsigned short allowed_repos;
 
 	ipkg = apk_pkg_get_installed(name);
 	if (!ipkg) return NULL;
 
-	if (!pkg0) {
-		foreach_array_item(p, name->providers) {
-			pkg0 = p->pkg;
-			if (pkg0 == ipkg) continue;
-			r = apk_version_compare_blob(*pkg0->version, *latest);
-			if (r == APK_VERSION_GREATER) latest = pkg0->version;
-		}
-	} else {
-		latest = pkg0->version;
-	}
-	return apk_version_compare_blob(*ipkg->version, *latest) == APK_VERSION_LESS ? ipkg : NULL;
+	allowed_repos = db->repo_tags[ipkg->ipkg->repository_tag].allowed_repos;
+	if (!(pkg->repos & allowed_repos)) return NULL;
+
+	return apk_version_compare_blob(*ipkg->version, *pkg->version) == APK_VERSION_LESS ? ipkg : NULL;
 }
 
-static void print_package(const struct apk_package *pkg, const struct list_ctx *ctx)
+static void print_package(const struct apk_database *db, const struct apk_package *pkg, const struct list_ctx *ctx)
 {
 	if (apk_verbosity <= 0) {
 		printf("%s\n", pkg->name->name);
@@ -108,13 +97,9 @@ static void print_package(const struct apk_package *pkg, const struct list_ctx *
 
 	if (pkg->ipkg)
 		printf(" [installed]");
-	else
-	{
-		const struct apk_package *u;
-
-		u = is_upgradable(pkg->name, pkg);
-		if (u != NULL)
-			printf(" [upgradable from: " PKG_VER_FMT "]", PKG_VER_PRINTF(u));
+	else {
+		const struct apk_package *u = is_upgradable(db, pkg);
+		if (u != NULL) printf(" [upgradable from: " PKG_VER_FMT "]", PKG_VER_PRINTF(u));
 	}
 
 
@@ -128,7 +113,7 @@ static void print_package(const struct apk_package *pkg, const struct list_ctx *
 	printf("\n");
 }
 
-static void filter_package(const struct apk_package *pkg, const struct list_ctx *ctx)
+static void filter_package(const struct apk_database *db, const struct apk_package *pkg, const struct list_ctx *ctx)
 {
 	if (ctx->match_origin && !origin_matches(ctx, pkg))
 		return;
@@ -142,13 +127,13 @@ static void filter_package(const struct apk_package *pkg, const struct list_ctx 
 	if (ctx->available && pkg->repos == BIT(APK_REPOSITORY_CACHED))
 		return;
 
-	if (ctx->upgradable && !is_upgradable(pkg->name, pkg))
+	if (ctx->upgradable && !is_upgradable(db, pkg))
 		return;
 
-	print_package(pkg, ctx);
+	print_package(db, pkg, ctx);
 }
 
-static void iterate_providers(const struct apk_name *name, const struct list_ctx *ctx)
+static void iterate_providers(const struct apk_database *db, const struct apk_name *name, const struct list_ctx *ctx)
 {
 	struct apk_provider *p;
 
@@ -160,7 +145,7 @@ static void iterate_providers(const struct apk_name *name, const struct list_ctx
 		if (ctx->match_providers)
 			printf("<%s> ", name->name);
 
-		filter_package(p->pkg, ctx);
+		filter_package(db, p->pkg, ctx);
 	}
 }
 
@@ -171,15 +156,14 @@ static void print_result(struct apk_database *db, const char *match, struct apk_
 	if (name == NULL)
 		return;
 
-	if (ctx->match_depends)
-	{
+	if (ctx->match_depends) {
 		struct apk_name **pname;
 
 		foreach_array_item(pname, name->rdepends)
-			iterate_providers(*pname, ctx);
+			iterate_providers(db, *pname, ctx);
+	} else {
+		iterate_providers(db, name, ctx);
 	}
-	else
-		iterate_providers(name, ctx);
 }
 
 #define LIST_OPTIONS(OPT) \
