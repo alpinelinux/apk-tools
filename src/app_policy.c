7ae9767b689c45dd5d0a3fb3fe343ce96e8319d4
Sertonix <sertonix@posteo.net>
2024-05-06 17:19:35 +0000

    policy: add source optgroup

--- src/app_policy.c
+++ src/app_policy.c
@@ -69,6 +69,7 @@ static int policy_main(void *ctx, struct apk_ctx *ac, struct apk_string_array *a
 static struct apk_applet apk_policy = {
 	.name = "policy",
 	.open_flags = APK_OPENF_READ,
+	.optgroups = { &optgroup_global, &optgroup_source },
 	.main = policy_main,
 };
 
