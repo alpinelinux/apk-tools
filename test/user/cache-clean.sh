fc66fdfc49cd7ca4fe63051f48d123c63789f839
q66 <q66@chimera-linux.org>
2024-12-18 19:26:13 +0100

    test: portability fixes

--- test/user/cache-clean.sh
+++ test/user/cache-clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source $(dirname "$0")/../testlib.sh
+. $(dirname "$0")/../testlib.sh
 
 setup_apkroot
 APK="$APK --allow-untrusted --no-interactive"
@@ -16,7 +16,7 @@ CACHED_A=$(glob_one "$TEST_ROOT/etc/apk/cache/test-a-1.0.*.apk")
 CACHED_B=$(glob_one "$TEST_ROOT/etc/apk/cache/test-b-1.0.*.apk")
 
 CACHED_B2="$TEST_ROOT/etc/apk/cache/test-b-1.0.xeeb78f1.apk"
-CACHED_C=${CACHED_B//test-b/test-c}
+CACHED_C=$(echo "$CACHED_B" | sed 's,test-b,test-c,')
 
 [ -f "$CACHED_A" ] || assert "cached test-a not preset"
 [ -f "$CACHED_B" ] || assert "cached test-b not preset"
@@ -24,7 +24,7 @@ CACHED_C=${CACHED_B//test-b/test-c}
 [ -f "$CACHED_C" ] && assert "cached test-c preset"
 
 touch $CACHED_C $CACHED_B2
-dd if=/dev/zero of=$CACHED_B bs=1024 count=1 >& /dev/null
+dd if=/dev/zero of=$CACHED_B bs=1024 count=1 > /dev/null 2>&1
 
 $APK cache clean -vv
 
--- test/user/mkndx.sh
+++ test/user/mkndx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source $(dirname "$0")/../testlib.sh
+. $(dirname "$0")/../testlib.sh
 
 setup_apkroot
 APK="$APK --allow-untrusted --no-interactive"
@@ -18,21 +18,21 @@ Index has 2 packages (of which 1 are new)
 EOF
 
 $APK mkndx --pkgname-spec 'https://test/${name}-${version}.apk' -o index.adb test-a-1.0.apk test-b-1.0.apk
-$APK fetch --url --simulate --from none --repository index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b >& fetch.log
+$APK fetch --url --simulate --from none --repository index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b > fetch.log 2>&1
 diff -u fetch.log - <<EOF || assert "wrong fetch result"
 https://test/test-a-1.0.apk
 https://test/test-b-1.0.apk
 EOF
 
 $APK mkndx --pkgname-spec '${name:3}/${name}-${version}.apk' -o index.adb test-a-1.0.apk test-b-1.0.apk
-$APK fetch --url --simulate --from none --repository file://localhost/$PWD/index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b >& fetch.log
+$APK fetch --url --simulate --from none --repository file://localhost/$PWD/index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b > fetch.log 2>&1
 diff -u fetch.log - <<EOF || assert "wrong fetch result"
 file://localhost/$PWD/tes/test-a-1.0.apk
 file://localhost/$PWD/tes/test-b-1.0.apk
 EOF
 
 $APK mkndx --pkgname-spec '${name:3}/${name}-${version}.apk' -o index.adb test-a-1.0.apk test-b-1.0.apk
-$APK fetch --url --simulate --from none --repository index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b >& fetch.log
+$APK fetch --url --simulate --from none --repository index.adb --pkgname-spec '${name}_${version}.pkg' test-a test-b > fetch.log 2>&1
 diff -u fetch.log - <<EOF || assert "wrong fetch result"
 ./tes/test-a-1.0.apk
 ./tes/test-b-1.0.apk
