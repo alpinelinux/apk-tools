c481c45ee5aad447144b2f09dabb0763015ea18c
Timo Teräs <timo.teras@iki.fi>
2026-01-27 10:47:27 +0200

    test: add test for replaces to function
    ref #11175

--- test/user/replaces.sh
+++ test/user/replaces.sh
@@ -4,9 +4,9 @@ TESTDIR=$(realpath "${TESTDIR:-"$(dirname "$0")"/..}")
 . "$TESTDIR"/testlib.sh
 
 create_pkg() {
-	local pkg="$1" ver="1.0"
+	local pkg="$1" ver="$2"
 	local pkgdir="files/"${pkg}-${ver}""
-	shift
+	shift 2
 
 	mkdir -p "$pkgdir"/files
 	echo "$pkg" > "$pkgdir"/files/test-file
@@ -23,12 +23,13 @@ check_content() {
 setup_apkroot
 APK="$APK --allow-untrusted --no-interactive"
 
-create_pkg a  -I "tags:tagA tagB"
-create_pkg b
-create_pkg c -I "replaces:a"
+create_pkg a 1.0  -I "tags:tagA tagB"
+create_pkg a 2.0  -I "tags:tagA tagB"
+create_pkg b 1.0
+create_pkg c 1.0 -I "replaces:a"
 
-create_pkg d-a -I "origin:d"
-create_pkg d-b -I "origin:d"
+create_pkg d-a 1.0 -I "origin:d"
+create_pkg d-b 1.0 -I "origin:d"
 
 $APK add --initdb $TEST_USERMODE a-1.0.apk
 check_content "a"
@@ -46,6 +47,8 @@ $APK add b-1.0.apk && assert "should error with conflicting file"
 check_content "a"
 $APK del b
 $APK add c-1.0.apk || assert "should succeed with replaces"
+check_content "c"
+$APK add a-2.0.apk || assert "a upgrade should succeed"
 check_content "c"
 $APK del a c
 
