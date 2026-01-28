c28debd2590c056d03c4736bee5bea972dfbada7
Timo Teräs <timo.teras@iki.fi>
2026-01-28 10:19:46 +0200

    portability: do not include_next sys/random.h if its missing
    sys/random.h is used only for the getrandom() and the whole
    header is likely missing if the function is absent. Fix the
    portability layer to not include_next it if shipping the compat
    version of getrandom().

--- portability/sys/random.h
+++ portability/sys/random.h
@@ -1,6 +1,7 @@
-#include_next <sys/random.h>
+#ifdef NEED_GETRANDOM
 #include <sys/types.h>
 
-#ifdef NEED_GETRANDOM
 ssize_t getrandom(void *buf, size_t buflen, unsigned int flags);
+#else
+#include_next <sys/random.h>
 #endif
