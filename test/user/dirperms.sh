6bd27cc84013c481bc106c5b66ffda660de82a95
Timo Teräs <timo.teras@iki.fi>
2026-08-31 11:48:53 +0300

    test: fix shellcheck warning for user/dirperms

--- test/user/dirperms.sh
+++ test/user/dirperms.sh
@@ -10,9 +10,9 @@ setup_apkroot
 APK="$APK --allow-untrusted --no-interactive"
 
 mkdir -p files
-mkdir -pm7777 files/testdir
+mkdir -m7777 files/testdir
 
 $APK mkpkg -I name:stickydir -I version:1.0 -F files -o stickydir-1.0.apk
 
 $APK add --initdb $TEST_USERMODE stickydir-1.0.apk
-[ "$(stat -c %a "$TEST_ROOT"/testdir)" == 7777 ] || assert "Wrong directory permissions"
+[ "$(stat -c %a "$TEST_ROOT"/testdir)" = 7777 ] || assert "Wrong directory permissions"
